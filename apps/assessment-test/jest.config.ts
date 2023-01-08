85400b371c6c2e31fbbff2c68039ce07b54d15fa
AndreinChef <nguyen.nguyen@savvycomsoftware.com>
2023-01-09 03:53:56 +0700

    Feat: Run format code with config husky, prettier and settings work spase

--- apps/assessment-test/jest.config.ts
+++ apps/assessment-test/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'assessment-test',
-  preset: '../../jest.preset.js',
+  displayName: "assessment-test",
+  preset: "../../jest.preset.js",
   transform: {
-    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
+    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
+    "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nrwl/react/babel"] }],
   },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/assessment-test',
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+  coverageDirectory: "../../coverage/apps/assessment-test",
 };
