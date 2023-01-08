015f907450c99b8eceb1abd6915898d0c1a5ee4a
AndreinChef <nguyen.nguyen@savvycomsoftware.com>
2023-01-09 04:56:41 +0700

    Feat: Login page, Home page and Provider

--- libs/web/src/index.ts
+++ libs/web/src/index.ts
@@ -1,1 +1,4 @@
+export * from "./lib/provider/provider";
+export * from "./lib/home-page/home-page";
+export * from "./lib/login-page/login-page";
 export * from "./lib/web";
---
+++ libs/web/src/lib/provider/provider.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ProviderProps {}
+
+export function Provider(props: ProviderProps) {
+  return (
+    <div>
+      <h1>Welcome to Provider!</h1>
+    </div>
+  );
+}
+
+export default Provider;
