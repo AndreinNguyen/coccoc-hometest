8d0b6f5fa44c6a5964b35c4aea62ef8a87ddb673
AndreinChef <nguyen.nguyen@savvycomsoftware.com>
2023-01-09 04:04:17 +0700

    Feat: Add web component

---
+++ libs/web/src/lib/web.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface WebProps {}
+
+export function Web(props: WebProps) {
+  return (
+    <div>
+      <h1>Welcome to Web!</h1>
+    </div>
+  );
+}
+
+export default Web;
