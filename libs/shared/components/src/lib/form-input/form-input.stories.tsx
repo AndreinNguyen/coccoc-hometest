8b7149b888b1aa529eb9209be41168241ba31ef3
AndreinChef <nguyen.nguyen@savvycomsoftware.com>
2023-01-09 12:42:55 +0700

    Feat: Implement FormInput component with Mui + React-hook-form

--- libs/shared/components/src/lib/form-input/form-input.stories.tsx
+++ libs/shared/components/src/lib/form-input/form-input.stories.tsx
@@ -1,15 +1,37 @@
+import { TextField } from "@mui/material";
 import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import { useForm } from "react-hook-form";
 import { FormInput } from "./form-input";
 
-const Story: ComponentMeta<typeof FormInput> = {
+const Story: ComponentMeta<typeof TextField> = {
   component: FormInput,
   title: "FormInput",
 };
 export default Story;
 
-const Template: ComponentStory<typeof FormInput> = (args) => (
-  <FormInput {...args} />
-);
+const Template: ComponentStory<typeof TextField> = (args) => {
+  const { control } = useForm({
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  return (
+    <FormInput
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      defaultValue={"" as any}
+      control={control}
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      name={"email" as any}
+      onChange={() => {
+        console.log("1");
+      }}
+      {...args}
+    />
+  );
+};
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  variant: "outlined",
+};
--- libs/shared/components/src/lib/form-input/form-input.tsx
+++ libs/shared/components/src/lib/form-input/form-input.tsx
@@ -1,11 +1,63 @@
-/* eslint-disable-next-line */
-export interface FormInputProps {}
+import { TextFieldProps, TextField, Box, Typography } from "@mui/material";
+import { Controller, FieldValues, UseControllerProps } from "react-hook-form";
+import CautionCircle from "../caution-circle/caution-circle";
 
-export function FormInput(props: FormInputProps) {
+/* eslint-disable-next-line */
+export type FormInputProps<T extends FieldValues> = UseControllerProps<T> &
+  TextFieldProps;
+
+export function FormInput<T extends FieldValues>(props: FormInputProps<T>) {
+  const {
+    control,
+    name,
+    InputProps,
+    type,
+    variant,
+    color,
+    placeholder,
+    rules,
+    ...rest
+  } = props;
+
   return (
-    <div>
-      <h1>Welcome to FormInput!</h1>
-    </div>
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      {...rest}
+      render={({
+        field: { onChange, value },
+        fieldState: { error },
+        formState,
+      }) => (
+        <TextField
+          helperText={
+            error ? (
+              <Box display={"flex"} alignItems="center">
+                <CautionCircle color="error" />
+                <Typography
+                  alignItems={"center"}
+                  fontSize={12}
+                  ml={0.5}
+                  mt={0.4}
+                >
+                  Require
+                </Typography>
+              </Box>
+            ) : null
+          }
+          error={!!error}
+          onChange={onChange}
+          value={value}
+          fullWidth
+          type={type}
+          variant={variant ?? "standard"}
+          color={color ?? "primary"}
+          placeholder={placeholder}
+          InputProps={InputProps}
+        />
+      )}
+    />
   );
 }
 
